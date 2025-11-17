2e7e8f66291acdac46b95392bec40edddd4e2b31
maddi-stinks <740546@student.dusd.net>
2025-11-17 11:57:31 -0800

    fixed up movement code
    added more tiles to tilemap aswell.

--- P4Flores2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
+++ P4Flores2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
@@ -6,10 +6,11 @@ using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
+    public InputAction MoveAction;
     // Start is called before the first frame update
     void Start()
     {
-
+        MoveAction.Enable();
     }
 
 
@@ -17,35 +18,9 @@ public class PlayerController : MonoBehaviour
     void Update()
 
     {
-        float horizontal = 0.0f;
-        if (Keyboard.current.leftArrowKey.isPressed)
-        {
-            horizontal = -1.0f;
-
-        }
-        else if (Keyboard.current.rightArrowKey.isPressed)
-
-        {
-            horizontal = 1.0f;
-        }
-
-        Debug.Log(horizontal);
-
-        float vertical = 0.0f;
-        if (Keyboard.current.upArrowKey.isPressed)
-        {
-            vertical = 1.0f;
-        }
-
-        else if (Keyboard.current.downArrowKey.isPressed)
-        {
-            vertical = -1.0f;
-        }
-        Debug.Log(vertical);
-
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
-        position.y = position.y + 0.1f * vertical;
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        Vector2 position = (Vector2)transform.position + move * 0.1f;
         transform.position = position;
     }
 }
