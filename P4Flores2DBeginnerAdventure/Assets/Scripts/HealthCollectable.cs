c33de6b8b7c8b2f2f693dbbdf116e479fa5a93c0
740546-oss <740546@student.dusd.net>
2025-11-28 19:53:57 -0800

    started damage zones
    fixed errors

--- P4Flores2DBeginnerAdventure/Assets/Scripts/HealthCollectable.cs
+++ P4Flores2DBeginnerAdventure/Assets/Scripts/HealthCollectable.cs
@@ -2,19 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealthCollectable : MonoBehaviour
+public class HealthCollectible : MonoBehaviour
 {
+
+
     void OnTriggerEnter2D(Collider2D other)
     {
         PlayerController controller = other.GetComponent<PlayerController>();
-        if (controller != null)
+
+
+        if (controller != null && controller.health < controller.maxHealth)
         {
-            if (controller.Health < controller.maxHealth)
-            {
-                controller.ChangeHealth(1);
-                Destroy(gameObject);
-            }
-            
-        }   
+            controller.ChangeHealth(1);
+            Destroy(gameObject);
+        }
     }
 }
--- P4Flores2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
+++ P4Flores2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
@@ -10,35 +10,49 @@ public class PlayerController : MonoBehaviour
     public InputAction MoveAction;
     Rigidbody2D rigidbody2d;
     Vector2 move;
-    public float speed = 10.0f;
+    public float speed = 3.0f;
 
-    // public InputAction Move;
-    // public float speed = 10.0f;
-    // Start is called before the first frame update
 
     // Variables related to the health system
     public int maxHealth = 5;
-    public int health { get { return Health; } }
-    int currenthealth;
+    int currentHealth;
+    public int health { get { return currentHealth; } }
 
 
+    // Variables related to temporary invincibility
+    public float timeInvincible = 2.0f;
+    bool isInvincible;
+    float damageCooldown;
+
+
+    // Start is called before the first frame update
     void Start()
     {
         MoveAction.Enable();
         rigidbody2d = GetComponent<Rigidbody2D>();
-       //currentHealth = maxHealth;
-    }
 
+
+        currentHealth = maxHealth;
+    }
 
     // Update is called once per frame
     void Update()
     {
         move = MoveAction.ReadValue<Vector2>();
-        Debug.Log(move);
+
+
+        if (isInvincible)
+        {
+            damageCooldown -= Time.deltaTime;
+            if (damageCooldown < 0)
+            {
+                isInvincible = false;
+            }
+        }
     }
 
 
-    // FixedUpdate has the same call rate as the physics system 
+    // FixedUpdate has the same call rate as the physics system
     void FixedUpdate()
     {
         Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
@@ -46,8 +60,19 @@ public class PlayerController : MonoBehaviour
     }
 
 
-   public void ChangeHealth(int amount)
+    public void ChangeHealth(int amount)
     {
+        if (amount < 0)
+        {
+            if (isInvincible)
+            {
+                return;
+            }
+            isInvincible = true;
+            damageCooldown = timeInvincible;
+        }
+
+
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
